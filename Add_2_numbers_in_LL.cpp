b04ab61ae5e401a669aa19dc4f1c9b34fca379f7
debojit366 <debojitdas366@gmail.com>
2025-03-25 09:30:03 +0530

    addition of 2 numbers in LL

---
+++ Add_2_numbers_in_LL.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+            ListNode* dummy = new ListNode(-1);
+            ListNode* current = dummy;
+            ListNode* temp1 = l1;
+            ListNode* temp2 = l2;
+            int carry = 0;
+            int sum;
+            while(temp1 || temp2){
+                sum = carry;
+                if(temp1){
+                    sum = sum + temp1->val;
+                }
+                if(temp2){
+                    sum = sum + temp2->val;
+                }
+               ListNode* newDummy = new ListNode(sum%10);
+               current->next = newDummy;
+               current = newDummy;
+               if(temp1) temp1 = temp1->next;
+               if(temp2) temp2 = temp2->next;
+               carry = sum/10;
+            }
+            if(carry){
+                ListNode* newDummy = new ListNode(carry);
+                current->next = newDummy;
+                current = newDummy;
+            }
+            return dummy->next;
+        }
+    };
